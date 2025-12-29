d88cc3567345c9fe3a4488a27abb362f4d8264bb
neko233 <1417015340@qq.com>
2025-12-29 10:33:17 +0800

    auto commit for version code

--- git 一键提交.cmd
+++ git 一键提交.cmd
@@ -1,3 +1,8 @@
 git add .
 git commit -m "auto commit for version code "
 git push origin main
+
+
+git remote rm github
+git remote add github https://github.com/neko233-com/config233-go.git
+git push github main
